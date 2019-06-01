cd8df95c5aea51fc44434d8fd4ac164bd7162753
Maicom M. Rodeghiero <maicom_mr@hotmail.com>
2019-06-01 16:40:51 -0300

    Views-Model-Controller-CRUD_WIP
    Criação das Models
    Criação do Controller e início dos trabalhos no CRUD
    Criação de views para a interação com o usuário

---
+++ resources/views/home_cliente.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="<?php echo asset('css/estiloBase.css') ?>" type="text/css">
+    <title>Home</title>
+
+</head>
+
+<body>
+    <div class="headerHome">
+        <h1>AeroSul - Revenda de Aeronaves </h1>
+    </div>
+
+    <div class="headerBuscar">
+
+        <select name="" id="">
+            <option value="EMBRAER">EMBRAER</option>
+            <option value="CESSNA">CESSNA</option>
+            <option value="PIPER">PIPER</option>
+        </select>
+
+        <input type="text"> <button type="submit">BUSCAR</button>
+    </div>
+
+
+<div class="cards">
+
+</div>
+
+</body>
+
+</html>
