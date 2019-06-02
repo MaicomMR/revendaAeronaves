a94f05fae3dd258d09a504f335b64d319af079e2
Maicom M. Rodeghiero <maicom_mr@hotmail.com>
2019-06-02 13:41:52 -0300

    Statistics-page-One
    Página de estatística configurada, gráfico básico elaborado com "Google Charts Pie"

--- app/ManufacturesModel.php
+++ app/ManufacturesModel.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\AirplanesModel;
 
 class ManufacturesModel extends Model
 {
-    //
+    protected $table = 'manufactures';
+
+    public function manufactureName(){
+        return $this->belongsTo('App\AirplanesModel', 'manufacture_id', 'id');
+    }
 }
